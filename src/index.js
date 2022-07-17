9944e9b843b58290b0cd6ce4a7df2dfdaeeff628
vladtarnovskiy <vladtarnovski989@gmail.com>
2022-07-18 01:12:54 +0300

    feat: function morse decoder

--- src/index.js
+++ src/index.js
@@ -35,12 +35,65 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+   let q = [];
+
+   for (let i = 0; i < expr.length; i+=10){
+        let catTenExpr = expr.substr(i, 10)
+        let pushPart = ''
+
+        for (let i = 0; i < catTenExpr.length; i+=2){
+            let letTwoExpr = catTenExpr.substr(i, 2)
+
+            if (letTwoExpr == '10'){
+                pushPart += '.'
+            } else if (letTwoExpr == '11'){
+                pushPart += '-'
+            } else if (letTwoExpr == '**'){
+                pushPart = ' '
+            }
+        }
+
+        q.push(pushPart)
+
+   }
+
+let result = q.map( item => MORSE_TABLE[item]).join('')
+   return result
+
 }
 
 module.exports = {
     decode
 }
+
+//ATTEMPTS with methot split 
+
+    // let r = []
+    // let z = expr.split('**********').forEach( (elem) => {
+    //    elem.split(/(.{10})/).split(/(.{2})/).forEach((k) => {
+    //         if (k == 10) {
+    //             r.push('.')
+    //         } else if (k == 11){
+    //             r.push('-')
+    //         }
+    //     })
+    // }
+    // )
+    // cosole.log (z)
+
+//     let x = expr.split('   ')
+//     let y  = [];
+
+// x.forEach((ob) => {
+//    ob.split(' ').forEach((el) => {
+//     y.push(MORSE_TABLE[el])
+//    })
+// })
+
+// return y.join(' ');
+// }
+
